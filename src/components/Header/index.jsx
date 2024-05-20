c2e64e28918f6473950a873e0b2dab4a5d10ba65
adryel martins <adryeldev@hotmail.com>
2024-05-20 15:09:54 -0300

    deploy imoveis

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -18,7 +18,7 @@ const Header = () => {
   return (
     <Container>
       <Logo>
-        <Link to="/">
+        <Link to="/SitemaDeAluguelDeImoveis">
           <img src={logoIMG} alt="Logo" />
         </Link>
       </Logo>
